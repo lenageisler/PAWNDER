609dc8144acc0b2ce8c9e1e13150a1156e31fbd2
Lena Geisler <lenageisler@gmx.de>
2025-09-02 15:31:45 +0200

    added helper methods for shelter?/searcher? to application_controller; added dogs index route, action, view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,10 @@
 class DogsController < ApplicationController
+  def index
+    if shelter?
+      @shelter_name = current_user.name
+      @dogs = current_user.dogs
+      raise
+    end
+  end
+
 end
