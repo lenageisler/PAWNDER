d8c0ff0522bf56f55c0f4bd33832efb8e7fc06a1
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-09-03 01:53:45 +0200

    build: Adds ToolCall functionality in related models to the AI Chat

--- app/models/ai_chat.rb
+++ app/models/ai_chat.rb
@@ -1,4 +1,6 @@
 class AiChat < ApplicationRecord
+  acts_as_chat
+
   belongs_to :user
   has_many :ai_messages, dependent: :destroy
   has_one :prefence, through: :user
--- app/models/ai_message.rb
+++ app/models/ai_message.rb
@@ -1,7 +1,10 @@
 class AiMessage < ApplicationRecord
+  acts_as_message
+
   belongs_to :ai_chat
   has_one :user, through: :ai_chat
 
   validates :role, presence: true
+  validates :ai_chat, presence: true
   validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
 end
