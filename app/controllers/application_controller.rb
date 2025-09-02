ae7bd67aafbff65bc57631f5ad651352d9b8b5c2
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-09-02 12:52:29 +0200

    added additional user fields to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :username, :name, :type, :location])
+
+    # For additional in app/views/devise/registrations/edit.html.erb -> to do?
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:role, :username, :name, :type, :location])
+  end
 end
