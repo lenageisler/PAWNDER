956c5b1ba88309f3dbd2921763e7e230639700cc
Lena Geisler <lenageisler@gmx.de>
2025-09-03 12:56:37 +0200

    added range filter in preference filter of dogs_controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -65,14 +65,17 @@ class DogsController < ApplicationController
   def preference_filter
     pref = current_user.preference
 
-    # age von bis
+    @dogs = @dogs.where("age >= ?", pref.age_min) if pref.age_min.present?
+    @dogs = @dogs.where("age <= ?", pref.age_max) if pref.age_max.present?
     @dogs = @dogs.where(gender: pref.gender) if pref.gender.present?
     @dogs = @dogs.where(neutered: pref.neutered) if pref.neutered.present?
     # breed grade multi
     # breed cat multi
     @dogs = @dogs.where("main_breed ILIKE ?", "%#{pref.main_breed}%") if pref.main_breed.present? #difficult to match - disclaimer in form??
-    # shoulder_height von bis
-    # weight von bis
+    @dogs = @dogs.where("shoulder_height >= ?", pref.shoulder_height_min) if pref.shoulder_height_min.present?
+    @dogs = @dogs.where("shoulder_height <= ?", pref.shoulder_height_max) if pref.shoulder_height_max.present?
+    @dogs = @dogs.where("weight >= ?", pref.weight_min) if pref.weight_min.present?
+    @dogs = @dogs.where("weight <= ?", pref.weight_max) if pref.weight_max.present?
     # location multi
     @dogs = @dogs.where(health_issus: pref.health_issus) if pref.health_issus.present?
     @dogs = @dogs.where(list_dog: pref.list_dog) if pref.list_dog.present?
