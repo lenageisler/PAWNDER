a4f9ea04886dee9aaac70962fbae4bd06e741472
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-09-03 09:32:03 +0200

    changed type to shelter_type in permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :username, :name, :type, :location])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :username, :name, :shelter_type, :location])
 
     # For additional in app/views/devise/registrations/edit.html.erb -> to do?
     # devise_parameter_sanitizer.permit(:account_update, keys: [:role, :username, :name, :type, :location])
