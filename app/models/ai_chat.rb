819f271f31d2609e67c95aa7e327d8f0a30b4a87
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-09-03 15:39:22 +0200

    fix: adds the correct associations and validations to the AiChat & AiMessage models

--- app/models/ai_chat.rb
+++ app/models/ai_chat.rb
@@ -1,7 +1,8 @@
 class AiChat < ApplicationRecord
-  acts_as_chat
+  acts_as_chat message_class: "AiMessage"
+  # message_class: sets Model with a name different from default one (Message)
 
   belongs_to :user
   has_many :ai_messages, dependent: :destroy
-  has_one :prefence, through: :user
+  has_one :preference, through: :user
 end
--- app/models/ai_message.rb
+++ app/models/ai_message.rb
@@ -1,10 +1,11 @@
 class AiMessage < ApplicationRecord
-  acts_as_message
+  acts_as_message chat_class: "AiChat", chat_foreign_key: "ai_chat_id"
+  # chat_class: & chat_foreign_key: sets the Chat with a different name than the default one (Chat)
 
   belongs_to :ai_chat
   has_one :user, through: :ai_chat
 
   validates :role, presence: true
-  validates :ai_chat, presence: true
+  # validates :ai_chat, presence: true # Arby told me this is not required
   validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
 end
