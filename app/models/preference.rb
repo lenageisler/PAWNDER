563f8e4b3aead146b02a90b84cc2e31eae669fd5
Lena Geisler <lenageisler@gmx.de>
2025-09-03 18:04:12 +0200

    deleted comment lines

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -10,10 +10,6 @@ class Preference < ApplicationRecord
   validates :weight_max, numericality: true, allow_blank: true
 
   #multiple choice
-  # validates :breed_grade, inclusion: { in: ["Pure breed", "Hybrid", "Mix"] }, allow_blank: true
-  # validates :breed_category, inclusion: { in: [ "Shepherd-type", "Herding-type", "Retriever-type", "Poudle-type", "Big gentle", "Sporty-Hunting-type", "Hound-type", "Spitz-type", "Compact & sturdy", "Terrier-type", "Small & fluffy", "Small & slick", "Sausage-type", "Medium mixed", "Large mixed", "Small mixed"] }, allow_blank: true
-  # validates :location, inclusion: { in: ["Shelter", "Foster Care", "Overseas"] }, allow_blank: true
-  # validates :ideal_environment, inclusion: { in: ["Apartment-friendly", "House with garden", "Countryside", "City life", "Quiet environment"] }, allow_blank: true
   validate :breed_grade_validation
   validate :breed_category_validation
   validate :location_validation
