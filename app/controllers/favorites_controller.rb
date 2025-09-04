673814464486f0f0b94e239348aa1ce2d454ecb3
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-09-04 14:32:19 +0200

    added view for destroy

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
       @favorite.dog = @dog
       @favorite.user = current_user
       if @favorite.save
-        redirect_to favorites_path, notice: "You have successfully added this dog to your favorites."
+        redirect_to favorites_path, notice: "You have successfully added #{@dog.name} to your favorites."
       else
         render "dogs/show", status: :unprocessable_entity
       end
@@ -24,7 +24,7 @@ class FavoritesController < ApplicationController
     if searcher?
       @favorite = Favorite.find(params[:id])
       @favorite.destroy
-      redirect_to root_path, alert: "Your preferences were deleted."
+      redirect_to favorites_path, alert: "#{@favorite.dog.name} was deleted from your favorites."
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 # Associations for SEARCHER users
   has_one :preference
   has_many :favorites
-  has_many :favorite_dogs, through: :favorites, source: :dogs
+  # has_many :favorite_dogs, through: :favorites, source: :dogs
   has_many :ai_chats, dependent: :destroy
   has_many :chats, through: :favorites, as: :searcher_chats
 
