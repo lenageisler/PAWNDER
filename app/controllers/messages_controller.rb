1a3a0edf7c6c831983c7272ce6c952d113b6a2cc
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-09-05 11:25:14 +0200

    feat: add create and send message with display in chat initial implementation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,20 @@
 class MessagesController < ApplicationController
 
+  def create
+    @chat = Chat.find(params[:chat_id])
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = current_user.role
+    if @message.save
+      redirect_to chat_path(@chat)
+    else
+      render "chats/show", status: :unprocessable_entity
+    end
+  end
 
-  
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
