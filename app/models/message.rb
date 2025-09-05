a142de04d5d7df251747c041bcc8620ac6b80666
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-09-05 13:01:58 +0200

    feat: add broadcasting messages via Websockets with Action Cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :chat
   has_one :favorite, through: :chat
+
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "chat_#{chat.id}_messages",
+                        partial: "messages/message",
+                        target: "messages",
+                        locals: { message: self, favorite: self.chat.favorite}
+  end
 end
