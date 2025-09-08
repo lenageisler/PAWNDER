c63501ab7ecbebce53994a522b91272ca596ca0c
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-09-08 09:33:36 +0200

    added dependent destroy for chats

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :dog
   has_one :shelter_user, through: :dog, source: :user
-  has_one :chat
+  has_one :chat, dependent: :destroy
   has_many :messages, through: :chat
 
   validates :user_id, uniqueness: { scope: :dog_id }
