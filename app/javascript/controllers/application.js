7017ff6395c237fb71beb85106f371b6934f6206
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-09-09 10:56:37 +0200

    docs: Add comments to explain the solution to the JavaScript bug in heroku

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,5 +8,8 @@ window.Stimulus   = application
 
 export { application }
 
+// ************** Heroku won't work if we add this lines. **********************
 // import ToggleSignupFieldsController from "./toggle_signup_fields_controller.js"
 // Stimulus.register("toggle-signup-fields", ToggleSignupFieldsController)
+// It is ok to delete this lines because this import is already handle by
+// 'eagerLoadControllersFrom' in the 'index.js'.
