49f23af747790b2d5d42628f6068f107025a6ffb
Lena Geisler <lenageisler@gmx.de>
2025-09-09 23:40:21 +0200

    finalized JS toggle for pref divs

--- app/javascript/controllers/toggle_pref_divs_controller.js
+++ app/javascript/controllers/toggle_pref_divs_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="toggle-pref-divs"
+// Connects to data-controller="toggle-form-div"
 export default class extends Controller {
-  static targets = ["]
+  static targets = ["body", "plus", "minus"]
 
   connect() {
   }
 
   toggle(event) {
-
+    this.bodyTarget.classList.toggle("d-none")
+    this.plusTarget.classList.toggle("d-none")
+    this.minusTarget.classList.toggle("d-none")
   }
 }
