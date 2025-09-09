3aad7fb153c21c23f71eb36cd8565e3bae0aebee
Lena Geisler <lenageisler@gmx.de>
2025-09-09 23:35:56 +0200

    stimulus controller for pref divs toggle

---
+++ app/javascript/controllers/toggle_pref_divs_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle-pref-divs"
+export default class extends Controller {
+  static targets = ["]
+
+  connect() {
+  }
+
+  toggle(event) {
+
+  }
+}
