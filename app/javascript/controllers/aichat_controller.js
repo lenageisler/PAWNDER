6fab10005c68a4ba363d931dd3b9e14478df43b8
Lena Geisler <lenageisler@gmx.de>
2025-09-10 23:54:56 +0200

    ai chat styling

--- app/javascript/controllers/aichat_controller.js
+++ app/javascript/controllers/aichat_controller.js
@@ -2,20 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="aichat"
 export default class extends Controller {
-  static targets = [ "window", "toggle", "xtoggle" ]
+  static targets = [ "window", "floatingQ", "xBtn" ]
   connect() {
   }
 
   open() {
     // event.preventDefault() // is here in case I want to use an <a>
     this.windowTarget.classList.remove("d-none")
-    this.toggleTarget.classList.add("d-none") // hide the button
-    this.xtoggleTarget.classList.remove("d-none") // shows the x button
+    this.floatingQTarget.classList.add("d-none") // hide the button
+    this.xBtnTarget.classList.remove("d-none") // shows the x button
   }
 
   close() {
     this.windowTarget.classList.add("d-none")
-    this.toggleTarget.classList.remove("d-none") // show the button again
-    this.xtoggleTarget.classList.add("d-none") // remove the x button
+    this.floatingQTarget.classList.remove("d-none") // show the button again
+    this.xBtnTarget.classList.add("d-none") // remove the x button
   }
 }
